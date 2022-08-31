fe7beb6456cb3f426a227adabc45cad2b961070e
PhonyLou <louqilin555@gmail.com>
2022-08-31 09:17:15 +0800

    can read from DB

--- src/main/java/com/tw/darkhorse/controller/DemoController.java
+++ src/main/java/com/tw/darkhorse/controller/DemoController.java
@@ -1,8 +1,10 @@
 package com.tw.darkhorse.controller;
 
 import com.tw.darkhorse.service.DemoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,15 @@ public class DemoController {
     @GetMapping("/foo")
     final public ResponseEntity foo() {
         var demoModel = demoService.issueInvoice();
-        System.out.println(demoModel);
         return ResponseEntity.ok(demoModel);
+    }
+
+    @GetMapping("/bar/{id}")
+    final public ResponseEntity bar(@PathVariable final Long id) {
+        var demoModel = demoService.findDemoBy(id);
+        if (demoModel.isPresent()) {
+            return ResponseEntity.ok(demoModel.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
--- src/main/java/com/tw/darkhorse/service/DemoService.java
+++ src/main/java/com/tw/darkhorse/service/DemoService.java
@@ -6,6 +6,8 @@ import com.tw.darkhorse.model.DemoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DemoService {
 
@@ -16,6 +18,11 @@ public class DemoService {
         var demoEntity = new DemoEntity(123L, "myName");
         DemoEntity savedEntity = demoRepository.save(demoEntity);
         return convertToModel(savedEntity);
+    }
+
+    public Optional<DemoModel> findDemoBy(final Long id) {
+        Optional<DemoEntity> demoEntity = demoRepository.findById(id);
+        return demoEntity.map(this::convertToModel);
     }
 
     private DemoModel convertToModel(final DemoEntity entity) {
