c48c4dbef7fa27cdb385ac10fe0c24117d1be21c
PhonyLou <louqilin555@gmail.com>
2022-08-31 23:27:05 +0800

    refactor migration timing

--- src/test/java/com/tw/darkhorse/outbound/database/DemoRepositoryTest.java
+++ src/test/java/com/tw/darkhorse/outbound/database/DemoRepositoryTest.java
@@ -2,14 +2,12 @@ package com.tw.darkhorse.outbound.database;
 
 
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.configuration.Configuration;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 @SpringBootTest
 public class DemoRepositoryTest {
@@ -20,22 +18,23 @@ public class DemoRepositoryTest {
     @Autowired
     private Flyway flyway;
 
+    @BeforeEach
+    void beforeEach() {
+        flyway.clean();
+        flyway.migrate();
+    }
+
     @Test
     @DisplayName("should return 1 when save entity")
     void a1() {
-        Configuration configuration = flyway.getConfiguration();
-        flyway.clean();
-        flyway.migrate();
-
         var inputEntity = new DemoEntity(1L, "test name");
         var savedEntity = demoRepository.save(inputEntity);
-        System.out.println(savedEntity);
         Assertions.assertEquals(inputEntity, savedEntity);
     }
 
-    @Test
-    void should_return_entity_when_query_by_id_given_data_exists() {
-        Optional<DemoEntity> entity = demoRepository.findById(1L);
-        Assertions.assertEquals(new DemoEntity(1L, "myName"), entity.get());
-    }
+//    @Test
+//    void should_return_entity_when_query_by_id_given_data_exists() {
+//        Optional<DemoEntity> entity = demoRepository.findById(1L);
+//        Assertions.assertEquals(new DemoEntity(1L, "myName"), entity.get());
+//    }
 }
