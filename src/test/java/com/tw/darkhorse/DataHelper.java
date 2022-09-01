241655157d190a3f0d8a658b4181940c493e2689
PhonyLou <louqilin555@gmail.com>
2022-09-01 19:07:57 +0800

    test controller by mock mvc

---
+++ src/test/java/com/tw/darkhorse/DataHelper.java
@@ -0,0 +1,17 @@
+package com.tw.darkhorse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tw.darkhorse.service.DemoModel;
+
+public abstract class DataHelper {
+    static ObjectMapper mapper = new ObjectMapper();
+
+    public static String asJsonString(Object o) throws JsonProcessingException {
+        return mapper.writeValueAsString(o);
+    }
+
+    public static DemoModel asTypeDemo(String s) throws JsonProcessingException {
+        return mapper.readValue(s, DemoModel.class);
+    }
+}
--- src/test/java/com/tw/darkhorse/controller/DemoControllerTest.java
+++ src/test/java/com/tw/darkhorse/controller/DemoControllerTest.java
@@ -2,33 +2,56 @@ package com.tw.darkhorse.controller;
 
 import com.tw.darkhorse.service.DemoModel;
 import com.tw.darkhorse.service.DemoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static com.tw.darkhorse.DataHelper.asJsonString;
+import static com.tw.darkhorse.DataHelper.asTypeDemo;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DemoControllerTest {
 
-    private DemoController demoController;
-    private final DemoService demoService = mock(DemoService.class);
+    @TestConfiguration
+    public static class Config {
+        @MockBean
+        DemoService demoService;
+    }
+
+    @Autowired
+    private DemoService demoService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
-        this.demoController = new DemoController(demoService);
     }
 
     @Test
-    public void given_when_then() {
+    public void given_when_then() throws Exception {
         DemoModel expectedDemoModel = new DemoModel(123L, "firstname");
+
         when(demoService.save(expectedDemoModel)).thenReturn(expectedDemoModel);
 
-        var responseEntity = demoController.foo(expectedDemoModel);
+        String contentAsString = mockMvc.perform(post("/demo/foo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedDemoModel))).andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat((DemoModel) responseEntity.getBody()).isEqualTo(expectedDemoModel);
+        DemoModel actualDemoModel = asTypeDemo(contentAsString);
+        Assertions.assertEquals(expectedDemoModel, actualDemoModel);
     }
 
 }
