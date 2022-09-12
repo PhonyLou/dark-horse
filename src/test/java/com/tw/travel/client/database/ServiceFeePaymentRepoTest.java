ab4dd6fce0efb697a9ae2da3f804842a52bb1e95
PhonyLou <louqilin555@gmail.com>
2022-09-12 12:24:00 +0800

    Story1 -> AC1 -> Example1 -> Work step 3

--- src/test/java/com/tw/travel/client/database/ServiceFeePaymentRepoTest.java
+++ src/test/java/com/tw/travel/client/database/ServiceFeePaymentRepoTest.java
@@ -1,10 +1,9 @@
 package com.tw.travel.client.database;
 
 
+import com.tw.helper.Story;
 import com.tw.travel.client.database.payment.ServiceFeePaymentEntity;
 import com.tw.travel.client.database.payment.ServiceFeePaymentRepo;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,37 +19,28 @@ public class ServiceFeePaymentRepoTest {
     @Autowired
     private ServiceFeePaymentRepo repo;
 
-    @Autowired
-    private Flyway flyway;
-
     @BeforeEach
     void beforeEach() {
-//        flyway.clean();
-//        flyway.migrate();
     }
 
-    /**
-     * Story1 AC1 Example1
-     */
+    @Story("Story1 -> AC1 -> Example1 -> Work step 3")
     @Test
     void should_return_travelId_with_pending_status_when_save_given_no_records_exist() {
-        LocalDate paymentRequestDate = LocalDate.parse("2022-09-11");
+        LocalDate paymentRequestDate = LocalDate.parse("2022-09-12");
         ServiceFeePaymentEntity expectedEntity = new ServiceFeePaymentEntity(1L, "pending", paymentRequestDate, paymentRequestDate.plusDays(5), paymentRequestDate);
         ServiceFeePaymentEntity actualSavedEntity = repo.save(expectedEntity);
         assertEquals(expectedEntity, actualSavedEntity);
     }
 
-    /**
-     * Story1 AC1 Example1
-     */
+    @Story("Story1 -> AC1 -> Example1 -> Work step 3")
     @Test
     void should_return_success_status_when_updateStatus_given_records_exist() {
-        LocalDate paymentRequestDate = LocalDate.parse("2022-09-11");
+        LocalDate paymentRequestDate = LocalDate.parse("2022-09-12");
         ServiceFeePaymentEntity expectedEntity = new ServiceFeePaymentEntity(1L, "pending", paymentRequestDate, paymentRequestDate.plusDays(5), paymentRequestDate);
         ServiceFeePaymentEntity savedEntity = repo.save(expectedEntity);
         assertEquals("pending", savedEntity.getStatus());
 
-        Integer successRecord = repo.updateStatus(1L, "success", paymentRequestDate.plusDays(1));
+        Integer successRecord = repo.updateStatus(1L, "success", paymentRequestDate);
         assertEquals(1, successRecord);
 
         ServiceFeePaymentEntity actualUpdatedEntity = repo.findById(1L).get();
@@ -58,7 +48,7 @@ public class ServiceFeePaymentRepoTest {
                         "success",
                         paymentRequestDate,
                         paymentRequestDate.plusDays(5),
-                        paymentRequestDate.plusDays(1)),
+                        paymentRequestDate),
                 actualUpdatedEntity);
     }
 
@@ -75,23 +65,4 @@ public class ServiceFeePaymentRepoTest {
         assertEquals("failed", repo.findById(1L).get().getStatus());
     }
 
-    @Test
-    void test_save_timestamp() {
-//        LocalDate createDay = LocalDate.parse("2022-09-12");
-//        ServiceFeePaymentEntityWithTime inputEntity = new ServiceFeePaymentEntityWithTime(1L,
-//                "pending", createDay, createDay.plusDays(5), createDay);
-//        ServiceFeePaymentEntityWithTime savedEntity = paymentRepo.save(inputEntity);
-//
-//        System.out.println(savedEntity);
-////        ServiceFeePaymentEntity expectedEntity = new ServiceFeePaymentEntity(1L, "pending");
-//        Assertions.assertEquals("pending", savedEntity.getStatus());
-//
-//        ServiceFeePaymentEntityWithTime successEntity = new ServiceFeePaymentEntityWithTime(1L,
-//                "success", null, null, createDay.plusDays(1));
-//        Integer result = paymentRepo.updateStatus(successEntity.getTravelContractId(), "success", successEntity.getLastUpdate());
-//        System.out.println("updated to ===> " + result);
-//
-//        ServiceFeePaymentEntityWithTime serviceFeePaymentEntityWithTime = paymentRepo.findById(1L).get();
-//        System.out.println("serviceFeePaymentEntityWithTime => " + serviceFeePaymentEntityWithTime);
-    }
 }
